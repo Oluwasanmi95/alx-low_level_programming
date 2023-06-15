34479fd492f4c011a5806378ad75ea4c1df9d445
Oluwasanmi <sanmioluwa95@gmail.com>
2023-06-15 10:28:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
+#include <string.h>
 /**
  * main - Prints a string to the standard error
- * Return: 0 (Success)
- */
+ * Return: 1 (Success)
+i*/
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(STDERR_FILENO, message, sizeof(message) - 1);
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, strlen(message));
 	return (1);
 }
