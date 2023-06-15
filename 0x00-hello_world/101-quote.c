f49ab8f3300b8e6e798f1fb88a291f273ef4f06c
Oluwasanmi <sanmioluwa95@gmail.com>
2023-06-15 10:48:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ int main(void)
 {
 	char *word = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, word, strlen(message));
+	write(2, word, strlen(word));
 	return (1);
 }
