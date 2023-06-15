969f567bb5cf2894e4aecb167ec85152b21618b1
Oluwasanmi <sanmioluwa95@gmail.com>
2023-06-15 10:33:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Prints a string to the standard error
  * Return: 1 (Success)
-i*/
+ */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, message, strlen(message));
 	return (1);
