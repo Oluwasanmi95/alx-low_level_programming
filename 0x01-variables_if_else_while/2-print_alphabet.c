a8024570695b97cc1d4f47d8a35e660dc1c0ceb1
Oluwasanmi <sanmioluwa95@gmail.com>
2023-06-16 07:37:03 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints each letter of alphabet
+ * Description: Program uses a for loop
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++;)
+	{
+		putchar('i');
+		putchar('\n');
+	}
+	return (0);
+}
