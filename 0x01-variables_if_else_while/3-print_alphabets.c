dc4a71f49bc8d6861b1b30978840cc8f489248e7
Oluwasanmi <sanmioluwa95@gmail.com>
2023-06-16 08:28:42 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints each letter of alphabet,lower and upper case
+ * Description: Program uses a for loop
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int i;
+	char a = 'a';
+	char b = 'A';
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a);
+		a++
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
