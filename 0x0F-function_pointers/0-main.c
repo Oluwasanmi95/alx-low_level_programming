a5ad008e95ae8e75a0a0ccf852a9793a312a337e
Oluwasanmi95 <131535474+Oluwasanmi95@users.noreply.github.com>
2023-07-22 10:46:48 +0100

    Update 0-main.c

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -47,6 +47,8 @@ int main(void)
 {
     print_name("Bob", print_name_as_is);
     print_name("Bob Dylan", print_name_uppercase);
+    print_name("Printf Project", print_name_uppercase);
+    print_name("Alx Holberton", print_name_uppercase);
     printf("\n");
     return (0);
 }
