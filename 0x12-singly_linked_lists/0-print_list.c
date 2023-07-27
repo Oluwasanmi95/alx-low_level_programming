7b8fd99a2e578707556ee2dff23bd35dae78cfda
Oluwasanmi <sanmioluwa95@gmail.com>
2023-07-27 12:38:25 -0700

    Myself

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,20 +9,14 @@
 size_t print_list(const list_t *h)
 {
 	const list_t *p = h;
-	size_t n = 2;
-
-	if (p->str == NULL)
-	{
-		printf("[0] (nil)\n");
-		printf("[5] World\n");
-		return (n);
-	}
-
-	n = 0;
+	size_t n = 0;
 
 	while (p != NULL)
 	{
-		printf("[%d] %s\n", p->len, p->str);
+		if (p->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", p->len, p->str);
 		p = p->next;
 		n++;
 	}
