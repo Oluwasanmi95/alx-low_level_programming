ab7c4e497674d4aa57a856a88805643a06f234e2
Oluwasanmi <sanmioluwa95@gmail.com>
2023-07-28 19:54:23 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * pre_main - Prints a string before the
+ *        main function is executed.
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
