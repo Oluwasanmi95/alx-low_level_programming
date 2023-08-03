f0915fb5e1613f4c114e7abf25f533c0f1422e71
Oluwasanmi <sanmioluwa95@gmail.com>
2023-08-03 09:33:24 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,14 +9,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask = 1UL << index;
+	unsigned long int temp = 1UL << index;
 
 	if (index >= sizeof(n) * 8)
 	{
 		return (-1);
 	}
 
-	if ((n & mask) != 0)
+	if ((n & temp) != 0)
 	{
 		return (1);
 	}
